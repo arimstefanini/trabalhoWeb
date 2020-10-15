dd000e42f2982c0a351ba8be15bc89e4c091b5cb
Rodrigo Silva <rodrigosilvarodrigues26@gmail.com>
2020-10-15 10:03:31 -0300

    atualização de js

--- js/classes/guests/ControllerGuests.js
+++ js/classes/guests/ControllerGuests.js
@@ -17,57 +17,61 @@ const info = document.querySelector("#info-table-text")
 
 export default class ControllerGuests {
 
-    model
-    lastOrder
+    #model
 
-    constructor(localBase) {
-        this.model = new ModelGuests(localBase)
-        this.lastOrder = ups[0]
+    constructor() {
+        this.#model = new ModelGuests(this)
 
         inFilter.addEventListener('input', () => this.upFilter())
         inNumber.addEventListener('input', () => this.upNumber())
 
-        regress.addEventListener('click', () => this.upPage(this.model.getPage() - 1))
-        next.addEventListener('click', () => this.upPage(this.model.getPage() + 1))
+        regress.addEventListener('click', () => this.upPage(this.#model.getPage() - 1))
+        next.addEventListener('click', () => this.upPage(this.#model.getPage() + 1))
 
         for (let i = 0; i < order.length; i++) {
-            ups[i].addEventListener('click', () => this.upOrder(order[i], ups[i], 1))
-            downs[i].addEventListener('click', () => this.upOrder(order[i], downs[i], -1))
+            ups[i].addEventListener('click', () => this.upOrder(order[i], 1))
+            downs[i].addEventListener('click', () => this.upOrder(order[i], -1))
         }
     }
 
     upPage(page) {
-        this.model.setPage(page)
+        this.#model.setPage(page)
         this.update()
     }
 
     upFilter() {
-        this.model.rebase()
-        this.model.filter(inFilter.value)
-        this.model.order()
-        this.model.setPage(1)
+        this.#model.rebase()
+        this.#model.filter(inFilter.value)
+        this.#model.order()
+        this.#model.setPage(1)
         this.update()
     }
 
     upNumber() {
-        this.model.setNumber(Number(inNumber.value))
-        this.model.updatePage()
+        this.#model.setNumber(Number(inNumber.value))
+        this.#model.updatePage()
         this.update()
     }
 
-    upOrder(field, button, direction) {
-        this.sortButtonColor(this.lastOrder, "#C0C0C0")
-        this.sortButtonColor(button, "#2880FB")
-        this.model.setOrder(field)
-        this.model.setOrderDirection(direction)
-        this.model.order()
-        this.lastOrder = button
+    upOrder(field, direction) {
+        const lastOrder = this.#getOrderButton(this.#model.getOrder(), this.#model.getOrderDirection())
+        const newOrder = this.#getOrderButton(field, direction)
+
+        this.#model.setOrder(field)
+        this.#model.setOrderDirection(direction)
+
+        this.#sortButtonColor(lastOrder, "#C0C0C0")
+        this.#sortButtonColor(newOrder, "#2880FB")
+
+        this.#model.order()
         this.update()
     }
+
+    #getOrderButton = (field, direction) => (direction === 1) ? ups[order.indexOf(field)] : downs[order.indexOf(field)];
 
     upInfo() {
-        info.textContent = 'Showing ' + Math.min(this.model.indexStart() + 1, this.model.indexEnd() + 1) + ' to ' + (this.model.indexEnd()+1) + ' of ' + this.model.size() + ' entries' +
-            (this.model.getFiltered() ? ' (filtered from ' + this.model.baseSize() + ' total entries)' : '')
+        info.textContent = 'Showing ' + (Math.min(this.#model.indexStart(), this.#model.indexEnd()) + 1) + ' to ' + (this.#model.indexEnd() + 1) + ' of ' + this.#model.size() + ' entries' +
+            (this.#model.getFiltered() ? ' (filtered from ' + this.#model.baseSize() + ' total entries)' : '')
     }
 
     upButtons() {
@@ -75,23 +79,23 @@ export default class ControllerGuests {
             buttons.firstChild.remove()
         }
 
-        const first = this.model.firstButton()
-        const last = this.model.lastButton()
+        const first = this.#model.firstButton()
+        const last = this.#model.lastButton()
 
         for (let i = first; i <= last; i++) {
-            buttons.appendChild(this.createButton(i));
+            buttons.appendChild(this.#createButton(i));
         }
 
         regress.blur()
-        regress.disabled = this.model.isFirstPage()
+        regress.disabled = this.#model.isFirstPage()
 
         next.blur()
-        next.disabled = this.model.isLastPage()
+        next.disabled = this.#model.isLastPage()
     }
 
     upTable() {
-        this.removeGuests()
-        this.addGuests(this.model.currentGuests())
+        this.#removeGuests()
+        this.#addGuests(this.#model.currentGuests())
     }
 
     update() {
@@ -100,8 +104,13 @@ export default class ControllerGuests {
         this.upButtons()
     }
 
+    /* head */
+    #sortButtonColor = (button, color) => (button.getAttribute('class').includes('on')) ? button.style.borderBottomColor = color : button.style.borderTopColor = color
+
+    /* head */
+
     /* table  */
-    addGuest(id, phone, firsName, lastName, email) {
+    #addGuest(id, phone, firsName, lastName, email) {
         let row = table.insertRow(table.rows.length);
         row.setAttribute('class', 'line-table ' + ((table.rows.length % 2 === 0) ? 'even' : 'odd'))
         const param = [id, phone, firsName, lastName, email]
@@ -112,11 +121,11 @@ export default class ControllerGuests {
         }
     }
 
-    addGuests(guests) {
-        guests.forEach((guest) => this.addGuest(guest.id, guest.phone, guest.firstName, guest.lastName, guest.email))
+    #addGuests(guests) {
+        guests.forEach((guest) => this.#addGuest(guest.id, guest.phone, guest.firstName, guest.lastName, guest.email))
     }
 
-    removeGuests() {
+    #removeGuests() {
         while (table.rows.length > 0) {
             table.deleteRow(0)
         }
@@ -125,9 +134,9 @@ export default class ControllerGuests {
     /* table  */
 
     /*   buttons  */
-    createButton(number) {
+    #createButton(number) {
         let button = document.createElement('button')
-        button.setAttribute('class', 'button button-normal ' + ((number === this.model.getPage()) ? ' current-page' : ''))
+        button.setAttribute('class', 'button button-normal ' + ((number === this.#model.getPage()) ? ' current-page' : ''))
         button.textContent = number
         button.addEventListener('click', () => this.upPage(Number(button.textContent)))
         return button
@@ -135,8 +144,4 @@ export default class ControllerGuests {
 
     /*   buttons  */
 
-    /* head */
-    sortButtonColor = (button, color) => (button.getAttribute('class').includes('on')) ? button.style.borderBottomColor = color : button.style.borderTopColor = color
-
-    /* head */
 }
